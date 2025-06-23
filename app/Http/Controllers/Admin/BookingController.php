cfb95a6c61537520d63398b7327b72484ba50f3a
PaoloVisone <polvis.pv.22@gmail.com>
2025-06-23 18:11:11 +0200

    feat: Add BookingController and implement basic CRUD methods for bookings

---
+++ app/Http/Controllers/Admin/BookingController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Booking;
+use App\Models\Event;
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $bookings = Booking::all();
+
+        // dd($bookings);
+
+        return view("bookings.index", compact('bookings'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Booking $booking)
+    {
+        $booking->load('event');
+        return view('bookings.show', compact('booking'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
