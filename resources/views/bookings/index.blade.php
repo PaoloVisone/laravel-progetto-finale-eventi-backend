04d003602ca30c21c72f324dc3ed40c3b8c7d245
PaoloVisone <polvis.pv.22@gmail.com>
2025-07-14 20:54:39 +0200

    refactor: Adjust table width in bookings and events views for better layout consistency

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -13,7 +13,7 @@
 </div>
 
 {{-- TABLE --}}
-<div style="width: 90%; margin: 0 auto;">
+<div style="width: 100%; margin: 0 auto;">
     <table style="width: 100%; border-collapse: collapse; text-align: center; margin: 20px 0;">
         <thead>
             <tr style="background-color: #f2f2f2;">
--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -13,8 +13,8 @@
 </div>
 
 {{-- TABLE --}}
-<div style="width: 90%; margin: 0 auto;">
-    <table style="width: 100%; border-collapse: collapse; text-align: center; margin: 20px 0;">
+<div style="width: 100%; margin: 0 auto;">
+    <table style="width: 100%; border-collapse: collapse; text-align: center; overflow-x:auto; margin: 20px 0;">
         <thead>
             <tr style="background-color: #f2f2f2;">
                 <th style="padding: 10px; border: 1px solid #ddd;">ID</th>
